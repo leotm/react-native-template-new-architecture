41997f777e871b2df4ecb7171d2e9fdbe46969c5
LeoTM <leo.m@hotmail.co.uk>
2020-04-18 18:47:05 +0100

    Run ESLint auto fix

--- App.tsx
+++ App.tsx
@@ -11,18 +11,17 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   ScrollView,
-  View,
-  Text,
   StatusBar,
+  StyleSheet,
+  Text,
+  View,
 } from 'react-native';
-
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
   DebugInstructions,
+  Header,
+  LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -3,11 +3,12 @@
  */
 
 import 'react-native';
-import React from 'react';
-import App from '../App';
 
+import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+import App from '../App';
 
 it('renders correctly', () => {
   renderer.create(<App />);
