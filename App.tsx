312b169f2e33c3b1269dc5ff5ed7874ac7058c67
LeoTM <leo.m@hotmail.co.uk>
2020-04-19 14:21:54 +0100

    Remove App.tsx template comments

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
+ * Spotifire
+ * https://github.com/leotm/spotifire
  *
  * @format
  */
