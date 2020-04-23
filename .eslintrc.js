0c4de14eb948b4f0c15baec5c21c0333d170981c
LeoTM <leo.m@hotmail.co.uk>
2020-04-23 19:47:31 +0100

    Reconfigure ESLint rules
    - Re-order extends to my preference, factoring in maintainance of libraries
    - Tidy plugins
    - Remove unneeded rules
    - eslint-disable for this template project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   root: true,
   extends: [
-    '@react-native-community',
-    'airbnb-typescript',
     'prettier',
-    'prettier/@typescript-eslint',
-    'react-native-typescript',
-    'plugin:react-hooks/recommended',
-    'plugin:react-native/all',
-    'plugin:prettier/recommended',
     'prettier/react',
-    'prettier/standard'
+    'prettier/standard',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:react-native/all',
+    'plugin:react-hooks/recommended',
+    'react-native-typescript',
+    'airbnb-typescript',
+    '@react-native-community',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -22,14 +22,9 @@ module.exports = {
   env: {
     'react-native/react-native': true
   },
-  plugins: ['react', 'react-native', '@typescript-eslint', 'simple-import-sort', 'detox', 'react-hooks'],
+  plugins: ['react', 'react-native', 'detox', '@typescript-eslint', 'react-hooks'],
   ignorePatterns: ['lib/', 'babel.config.js', 'metro.config.js', 'tsconfig.json'],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'import/no-cycle': 'off',
-    'sort-keys': 'off',
-    'import-order-alphabetical/order': 'off',
-    'simple-import-sort/sort': 'warn',
-    'eslint-comments/no-unused-disable': 'off'
+    '@typescript-eslint/explicit-function-return-type': 'off'
   },
 };
--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+
 /**
  * Spotifire
  * https://github.com/leotm/spotifire
