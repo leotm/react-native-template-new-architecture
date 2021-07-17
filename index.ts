9a039843af835a7ca821319e275ca881c2a58d15
LeoTM <1881059+leotm@users.noreply.github.com>
2021-07-17 16:59:28 +0100

    Fix ESLint error import/no-named-as-default
    Error: Using exported name 'App' as identifier for default export
    
    Worth renaming and keeping this rule for now, for clarity benefits elaborated in rule rationale.
    
    Considered keeping App as entry/root name and removing App named export syntax, but then unit tests also rely on importing the default export, which relies on a config env var (currently temp boolean), just to build/run app or storybook.
    
    Despite likely about to implement config env vars anyway, better to rely on less things and stay flexible.

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@
 
 import { AppRegistry } from 'react-native'
 
-import App from './App'
+import Root from './App'
 import { name } from './app.json'
 
-AppRegistry.registerComponent(name, () => App)
+AppRegistry.registerComponent(name, () => Root)
