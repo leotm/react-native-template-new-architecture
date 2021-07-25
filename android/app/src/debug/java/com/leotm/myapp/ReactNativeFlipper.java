1f6e325f7a1d93101b4094cbae7a94427bace77c
LeoTM <1881059+leotm@users.noreply.github.com>
2021-07-25 17:06:44 +0100

    Set up Flipper 0.99 on Android

---
+++ android/app/src/debug/java/com/leotm/myapp/ReactNativeFlipper.java
@@ -0,0 +1,18 @@
+import android.content.Context;
+import com.facebook.flipper.android.AndroidFlipperClient;
+import com.facebook.flipper.android.utils.FlipperUtils;
+import com.facebook.flipper.core.FlipperClient;
+import com.facebook.flipper.plugins.inspector.DescriptorMapping;
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin;
+import com.facebook.react.ReactInstanceManager;
+import okhttp3.OkHttpClient;
+
+public class ReactNativeFlipper {
+  public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+    if (FlipperUtils.shouldEnableFlipper(context)) {
+      final FlipperClient client = AndroidFlipperClient.getInstance(context);
+
+      client.addPlugin(new InspectorFlipperPlugin(context, DescriptorMapping.withDefaults()));
+    }
+  }
+}
