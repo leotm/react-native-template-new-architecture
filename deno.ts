12bc70c72c68c6ae5218a0723955aa635c5cdc37
LeoTM <1881059+leotm@users.noreply.github.com>
2021-10-03 17:45:50 +0100

    Add basic equivalent Deno server example with comments

---
+++ deno.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { serve } from 'https://deno.land/std@0.106.0/http/server.ts'
+
+const server = serve({ port: 1337 })
+
+console.log('http://localhost:1337')
+
+// TODO: Enable VSCode Deno plugin only for this file, not codebase
+
+// eslint-disable-next-line no-restricted-syntax
+for await (const req of server) {
+  req.respond({ body: 'leet' })
+}
