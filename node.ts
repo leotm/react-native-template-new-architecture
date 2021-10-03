e4b384cd154323e17a08ff5202acbc07a83a3aae
LeoTM <1881059+leotm@users.noreply.github.com>
2021-10-03 11:30:31 +0100

    Update Node server data example for TypeScript resolution in CI

--- node.ts
+++ node.ts
@@ -2,8 +2,6 @@
 import { Application, Context } from '@curveball/core'
 import router from '@curveball/router'
 
-import data from './data'
-
 const app = new Application()
 
 console.log('http://localhost:1337 or http://127.0.0.1:1337')
@@ -12,7 +10,7 @@ console.log('Check: $ netstat -an')
 app.use(
   router('/data', (ctx: Context) => {
     ctx.status = 200
-    ctx.response.body = data
+    ctx.response.body = ['l', 'e', 'e', 't']
   })
 )
 
