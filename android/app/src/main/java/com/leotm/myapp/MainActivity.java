0896875746f5a0a88d6d2a8ae2ca4e1e4d87fc3b
LeoTM <1881059+leotm@users.noreply.github.com>
2021-10-11 13:29:38 +0100

    Config react-native-screens on Android

--- android/app/src/main/java/com/leotm/myapp/MainActivity.java
+++ android/app/src/main/java/com/leotm/myapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.leotm.myapp;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is
