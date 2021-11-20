0e9f21202603fe94697d60c35abe88a9b69b7c39
LeoTM <1881059+leotm@users.noreply.github.com>
2021-11-20 17:21:00 +0000

    Set @babel/preset-react runtime to automatic and document
    Auto import needed react/jsx-runtime fn's like React

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,24 @@
 */
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset']
+  presets: [
+    'module:metro-react-native-babel-preset',
+    [
+      '@babel/preset-react',
+      {
+        /**
+         * runtime: automatic
+         * - auto imports fn's JSX transpiles to like React
+         * - adds __source and __self props too for debugging
+         */
+        runtime: 'automatic', // default to classic
+        /**
+         * development: true
+         * - adds __source and __self props too for debugging
+         * - can't use with runtime: automatic from dupe props errors
+         * - runtime: classic means no auto imports
+         */
+      }
+    ]
+  ]
 }
