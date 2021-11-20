aa9ee5b8b71a46c7578665462414b1dc73a4e53f
LeoTM <1881059+leotm@users.noreply.github.com>
2021-11-20 16:36:59 +0000

    Document Metro RN Babel preset syntax and transform plugins

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+/**
+* metro-react-native-babel-preset includes
+* - @babel/preset-react jsx syntax plugin and transform plugins
+* - @babel/preset-typescript transform plugin
+*/
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset']
 }
