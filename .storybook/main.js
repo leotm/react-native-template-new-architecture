41c5e7df5c5b55607ffc030492cf39be1b4c5659
LeoTM <1881059+leotm@users.noreply.github.com>
2022-02-09 15:53:32 +0000

    Add @storybook/addon-ondevice-knobs to config and document
    - Docs 12mo OOD w 404 example
    - Get working then PR update
    - Should be pos since @storybook/addon-ondevice-knobs@npm:next resolving to 6.0.0-alpha.0
    - https://storybook.js.org/addons/@storybook/addon-ondevice-knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,12 @@ module.exports = {
     '@storybook/addon-ondevice-actions',
     '@storybook/addon-ondevice-backgrounds',
     '@storybook/addon-ondevice-controls',
-    '@storybook/addon-ondevice-notes' // NB: https://github.com/storybookjs/react-native/issues/24#issuecomment-615934280
+    '@storybook/addon-ondevice-notes', // NB: https://github.com/storybookjs/react-native/issues/24#issuecomment-615934280
+    /**
+     * NB: Docs 12mo OOD w 404 example, get working then PR update
+     * Should be pos since @storybook/addon-ondevice-knobs@npm:next resolving to 6.0.0-alpha.0
+     * https://storybook.js.org/addons/@storybook/addon-ondevice-knobs
+     */
+    '@storybook/addon-ondevice-knobs'
   ]
 }
