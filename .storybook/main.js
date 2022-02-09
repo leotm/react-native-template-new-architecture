e8417d3771073d45ffeebccf42df97a70812f816
LeoTM <1881059+leotm@users.noreply.github.com>
2022-02-09 15:43:11 +0000

    Document storybook main js config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
-// TODO: .ts
+/**
+ * @storybook/react-native/scripts (loader.js)
+ * ES6 mods not (yet) supported, for e.g.
+ * main.{ts/mjs}: export default { ... }
+ */
 module.exports = {
   stories: [],
   addons: [
     '@storybook/addon-ondevice-actions',
     '@storybook/addon-ondevice-backgrounds',
     '@storybook/addon-ondevice-controls',
-    '@storybook/addon-ondevice-notes' // https://github.com/storybookjs/react-native/issues/24#issuecomment-615934280
+    '@storybook/addon-ondevice-notes' // NB: https://github.com/storybookjs/react-native/issues/24#issuecomment-615934280
   ]
 }
