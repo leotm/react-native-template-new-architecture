7e4fa4bf60f8170db0406843241a50ac3574e8f2
LeoTM <1881059+leotm@users.noreply.github.com>
2022-02-10 16:08:01 +0000

    Document storybook main js config further

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,14 +5,23 @@
  */
 module.exports = {
   addons: [
-    '@storybook/addon-ondevice-actions',
+    /**
+     * The old way was to register these directly in v5.3
+     * Now get-stories codegens them to storybook.requires.js
+     * We used to import the deviceless addons too, now only dev/peerDeps
+     * (All are still direct dependencies of @storybook/addons)
+     * @deprecated @storybook/addon-notes (now @storybook/addon-ondevice-notes)
+     * @todo @storybook/addon-links example then PR to storybookjs/react-native
+     */
+    '@storybook/addon-ondevice-actions', // dev/peerDep @storybook/addon-actions
     '@storybook/addon-ondevice-backgrounds',
-    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-controls', // devDep @storybook/addon-knobs, peerDep @storybook/addon-controls
     '@storybook/addon-ondevice-notes', // NB: https://github.com/storybookjs/react-native/issues/24#issuecomment-615934280
     /**
      * NB: Docs 12mo OOD w 404 example, get working then PR update
      * Should be pos since @storybook/addon-ondevice-knobs@npm:next resolving to 6.0.0-alpha.0
      * https://storybook.js.org/addons/@storybook/addon-ondevice-knobs
+     * @todo @storybook/addon-ondevice-knobs example then PR to storybookjs/react-native
      */
     '@storybook/addon-ondevice-knobs'
   ],
