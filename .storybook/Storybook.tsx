7f38ef28d28edf1c1884609515101016bff733c9
LeoTM <1881059+leotm@users.noreply.github.com>
2022-02-14 15:58:45 +0000

    Document StorybookUIRoot getStorybookUI options

--- .storybook/Storybook.tsx
+++ .storybook/Storybook.tsx
@@ -2,6 +2,7 @@ import './storybook.requires'
 
 import { getStorybookUI } from '@storybook/react-native'
 
+// https://github.com/storybookjs/react-native#getstorybookui-options
 const StorybookUIRoot = getStorybookUI({})
 
 export default StorybookUIRoot
