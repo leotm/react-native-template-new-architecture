dcc2477094f7f06a510611e27776a380e8f80bd5
LeoTM <1881059+leotm@users.noreply.github.com>
2022-03-03 17:10:10 +0000

    Try RN nightly with Fabric, @react-navigation and react-native-screens

--- src/index.tsx
+++ src/index.tsx
@@ -1,126 +1,43 @@
-/* eslint-disable react-native/no-raw-text, no-console */
-// https://github.com/Intellicode/eslint-plugin-react-native/issues/271
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { I18nManager } from 'react-native'
+import chapters from './chapters.json'
+import Chapter from './Chapter'
+import HeaderDemo from './HeaderDemo'
+import { WHITE } from './colors'
 
-import type { FC } from 'react'
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View
-} from 'react-native'
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen'
+const Stack = createNativeStackNavigator()
 
-import StorybookUIRoot from '../.storybook/Storybook'
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerHideBackButton: true,
+        direction: I18nManager.isRTL ? 'rtl' : 'ltr'
+      }}
+    >
+      <Stack.Screen
+        name="Chapter"
+        options={{
+          title: 'Fabric Example',
+          headerShown: false
+        }}
+        initialParams={{
+          index: 0,
+          chapters: chapters,
+          chapterRoute: 'Chapter',
+          afterChapterRoute: 'HeaderDemo'
+        }}
+        component={Chapter}
+      />
+      <Stack.Screen
+        name="HeaderDemo"
+        component={HeaderDemo}
+        options={{ title: 'Header Demo', headerTintColor: WHITE }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+)
 
-if (__DEV__) {
-  import('../ReactotronConfig')
-    .then(() => {
-      console.log('Reactotron Configured')
-    })
-    .catch(() => console.error)
-}
-
-const Section: FC<{ title: string }> = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark'
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          { color: isDarkMode ? Colors.white : Colors.black }
-        ]}
-      >
-        {title}
-      </Text>
-
-      <Text
-        style={[
-          styles.sectionDescription,
-          { color: isDarkMode ? Colors.light : Colors.dark }
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  )
-}
-
-export const App = () => {
-  const isDarkMode = useColorScheme() === 'dark'
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
-  }
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <Header />
-
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white
-          }}
-        >
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
-
-const styles = StyleSheet.create({
-  highlight: {
-    fontWeight: '700'
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionDescription: {
-    fontSize: 18,
-    fontWeight: '400',
-    marginTop: 8
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600'
-  }
-})
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-constant-condition
-export default false ? StorybookUIRoot : App
-
-// export default Reactotron.storybookSwitcher(storybook)(App)
-// https://github.com/infinitered/reactotron/issues/1160
+export default App
