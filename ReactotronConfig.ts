3e776bf3df3e8128d2a7da3ec80308125ee57101
LeoTM <1881059+leotm@users.noreply.github.com>
2022-04-25 12:00:19 +0100

    Remove AsyncStorage from Reactotron config

--- ReactotronConfig.ts
+++ ReactotronConfig.ts
@@ -1,9 +1,7 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Reactotron from 'reactotron-react-native'
 
-Reactotron.setAsyncStorageHandler?.(AsyncStorage)
-  .configure() // controls connection & communication settings
+Reactotron.configure() // controls connection & communication settings
   .useReactNative({
     storybook: true
   }) // add all built-in react native plugins
