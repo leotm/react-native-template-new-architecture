41ed14b03fe5143f068fb3cb5dd4d50e99b79f34
LeoTM <1881059+leotm@users.noreply.github.com>
2022-08-03 18:58:35 +0100

    Disable Metro experimentalImportSupport

--- metro.config.js
+++ metro.config.js
@@ -8,11 +8,11 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         /**
-        * "If you are brave, turn the two flags to `true` which may bring significant perf improvements"
-        * More: https://github.com/zertosh/babel-plugin-transform-inline-imports-commonjs#details
-        * May require disabling with other libraries like Storybook, React Navigation, etc until supported.
-        */
-        experimentalImportSupport: true,
+         * "If you are brave, turn the two flags to `true` which may bring significant perf improvements"
+         * More: https://github.com/zertosh/babel-plugin-transform-inline-imports-commonjs#details
+         * May require disabling with other libraries like Storybook, React Navigation, etc until supported.
+         */
+        experimentalImportSupport: false,
         inlineRequires: true
       }
     })
