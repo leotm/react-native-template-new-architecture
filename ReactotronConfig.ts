f2e46374c4f98e385a40734ee623c59b9d25671d
LeoTM <1881059+leotm@users.noreply.github.com>
2022-12-22 13:42:09 +0000

    Revert changes up to 6c6791f6

--- ReactotronConfig.ts
+++ ReactotronConfig.ts
@@ -1,7 +1,9 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Reactotron from 'reactotron-react-native'
 
-Reactotron.configure() // controls connection & communication settings
+Reactotron.setAsyncStorageHandler?.(AsyncStorage)
+  .configure() // controls connection & communication settings
   .useReactNative({
     storybook: true
   }) // add all built-in react native plugins
--- babel.config.js
+++ babel.config.js
@@ -6,35 +6,35 @@
 
 module.exports = {
   presets: [
-    'module:metro-react-native-babel-preset'
-    // [
-    //   '@babel/preset-react',
-    //   {
-    //     /**
-    //      * runtime: automatic
-    //      * - auto imports fn's JSX transpiles to like React
-    //      * - adds __source and __self props too for debugging
-    //      */
-    //     runtime: 'automatic' // default to classic
-    //     /**
-    //      * development: true
-    //      * - adds __source and __self props too for debugging
-    //      * - can't use with runtime: automatic from dupe props errors
-    //      * - runtime: classic means no auto imports
-    //      */
-    //   }
-    // ]
+    'module:metro-react-native-babel-preset',
+    [
+      '@babel/preset-react',
+      {
+        /**
+         * runtime: automatic
+         * - auto imports fn's JSX transpiles to like React
+         * - adds __source and __self props too for debugging
+         */
+        runtime: 'automatic' // default to classic
+        /**
+         * development: true
+         * - adds __source and __self props too for debugging
+         * - can't use with runtime: automatic from dupe props errors
+         * - runtime: classic means no auto imports
+         */
+      }
+    ]
   ],
   plugins: [
-    // [
-    /**
-     * Allow custom env vars from Metro
-     * e.g. RN_ENV=staging react-native start --reset-cache
-     * Accessible from process.env.RN_ENV
-     */
-    // 'transform-inline-environment-variables'
-    // ]
-    // ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }]
+    [
+      /**
+       * Allow custom env vars from Metro
+       * e.g. RN_ENV=staging react-native start --reset-cache
+       * Accessible from process.env.RN_ENV
+       */
+      'transform-inline-environment-variables'
+    ],
+    ['babel-plugin-react-docgen-typescript', { exclude: 'node_modules' }]
     // 'react-native-reanimated/plugin'
   ]
 }
